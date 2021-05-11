4eeddb350bc4cf63223b6a6d4f926cc68d3ce9b8
ec-son <useniecson@gmail.com>
2021-05-11 10:23:32 +0200

    initialisation du projet

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui ipsa odio ullam, quia quo reprehenderit magnam officia voluptatibus impedit blanditiis? Doloremque dolorum quod, minus ab earum repellat beatae quam in?
+    <NuxtLink :to="'/helloworld'"> Cliquer ici ok</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'header'
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
